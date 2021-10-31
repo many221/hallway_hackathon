12721e36e5ad104c10d94bb7a128b85701945fa3
Manny Lee <many221@gmail.com>
2021-10-30 19:12:29 -0700

    SpringBOOT Folder

---
+++ Hallway/src/main/java/com/hallway/Hallway/HallwayApplication.java
@@ -0,0 +1,13 @@
+package com.hallway.Hallway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallwayApplication.class, args);
+	}
+
+}
---
+++ Hallway/src/test/java/com/hallway/Hallway/HallwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallway.Hallway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
